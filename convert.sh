f2ff506700f95460e34c1ee6ef080ec2afde21c0
Kevin Cooper <kevincooper@arizona.edu>
2020-09-14 14:54:41 -0700

    fix ssh_url

--- convert.sh
+++ convert.sh
@@ -5,11 +5,11 @@ GH_API="https://api.github.com/orgs/${GH_ORG}/repos?per_page=200"
 ISSUE_BRANCH='issues/311'
 
 mkdir -p repos cache
-curl -s "${GH_API}" | jq -r -c '.[] | {name: .name, git_url: .git_url}' > cache/gh_data.ndjson
+curl -s "${GH_API}" | jq -r -c '.[] | {name: .name, ssh_url: .ssh_url}' > cache/gh_data.ndjson
 
 # Clone all the repos in our org
 while read -r REPO_INFO; do
-  git clone --depth=1 "$(echo ${REPO_INFO} | jq -r '.git_url')" "repos/$(echo ${REPO_INFO} | jq -r '.name' | sed 's/^\.//g')"
+  git clone --depth=1 "$(echo ${REPO_INFO} | jq -r '.ssh_url')" "repos/$(echo ${REPO_INFO} | jq -r '.name' | sed 's/^\.//g')"
 done < cache/gh_data.ndjson
 
 # Set up main and issue branch, delete master
@@ -21,8 +21,10 @@ for ROOT in */ ; do
     git checkout master
     git pull
     git checkout -b main
+    # git push --set-upstream origin main
     git branch -d master
     git checkout -b "${ISSUE_BRANCH}"
+    # git push --set-upstream origin "${ISSUE_BRANCH}"
   )
 done
 )
